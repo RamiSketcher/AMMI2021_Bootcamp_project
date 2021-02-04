900e794526aabe80f5c72221899b1bfba90e89a1
RamiSketcher <ramisketcher@gmail.com>
2021-02-04 17:47:46 +0000

    Add main code

--- main.py
+++ main.py
@@ -1,41 +1,50 @@
-from utils import read_data
-from utils import read_test
+from utils import reader
 from model import RuleBasedModel
 
 
 def main():
 
-    train_file = 'path/to/train_data.txt'
-    test_file = 'path/to/test_data.txt'
+    train_file = 'Data/train_data.txt'
+    test_file = 'Data/test_data.txt'
     variables = ['ID', 'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9', 'f10', 'f11', 'class']
+    #
+    #
+    print("========= Reading train dataset =========")
+    # TO DO:
+    # use the read data function you created to read the train data
+    train_data = reader(train_file, variables)
+    print("======== Done reading =========.\n")
 
 
-    print ("========= Reading train dataset =========")
-    	# TO DO:
-	# use the read data function you created to read the train data
-    print ("======== Done reading =========.\n")
+    print("========= Reading test data =========")
+    # TO-DO
+    # Read the test  data
+    test_data = reader(test_file, variables)
+    print("========= Done reading =========.\n")
 
-    print ("========= Reading test data =========")
-    	# TO-DO 
-	# Read the test  data
-    print ("========= Done reading =========.\n")
 
-    print ("==== Training classifier =====")
-	# TO-DO
-	# Initialize the classifier you built in model.py and return the necessary values
-    print ("======== Done training classifier ===========.\n")
 
-    print ("========= Classifying test samples =======")
-	# TO-DO 
-	# use your classifier to do predictions on all the test samples
-    print ("========== Done classifying =======")
 
-    	# TO-DO
-	# Evalutate your classifier with the Accuracy function you implemented and return the necessary outputs
+    print("==== Training classifier =====")
+    # TO-DO
+    # Initialize the classifier you built in model.py and return the necessary values
+    model = RuleBasedModel()
+    print("======== Done training classifier ===========.\n")
+
+
+    print("========= Classifying test samples =======")
+    # TO-DO
+    # use your classifier to do predictions on all the test samples
+    predictions = model.predict_all(test_data)
+    for i in range(len(predictions)):
+            test_data[i]['prediction'] = predictions[i]
+    print("========== Done classifying =======")
+
+    # TO-DO
+    # Evalutate your classifier with the Accuracy function you implemented and return the necessary outputs
+    accuracy,numCorrect,total_samples = model.accuracy(test_data)
     print(f"Model's Accuracy {round(accuracy)} %, model correctly predicted {numCorrect} out of {total_samples}")
     print('================================================================')
-
-
-    print ("finished.\n")
+    print("finished.\n")
 
 main()
